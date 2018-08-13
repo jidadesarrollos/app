7fc908b2d9931e80e3cdc93ecb49cbd09f52b684
raynexus <raynexus@gmail.com>
2018-08-12 23:35:56 -0400

    ajustes traductor

--- Aplicacion/Controllers/App.php
+++ Aplicacion/Controllers/App.php
@@ -7,13 +7,26 @@
 
 namespace App\Controllers;
 
+use Jida\Componentes\Traductor;
+use Jida\Helpers\Debug;
+
 class App extends \Jida\Core\Controller {
 
     function __construct () {
 
         parent::__construct();
-        
+
         $this->layout('principal.tpl.php');
+
+        if (!$this->idioma) {
+            $this->idioma = "es";
+        }
+
+        $traductor = new Traductor($this->idioma);
+
+        $this->data([
+                        "traductor" => $traductor
+                    ]);
 
     }
 
--- Aplicacion/Vistas/index/index.php
+++ Aplicacion/Vistas/index/index.php
@@ -1,4 +1,4 @@
 <div class="jumbotron">
-    <h2>Plantilla básica JiDA Framework</h2>
+    <h2><?= $this->cadena('titulo', 'home') ?></h2>
     <p>Use esta plantilla para iniciar de forma rápida el desarrollo de un sitio web.</p>
 </div>
